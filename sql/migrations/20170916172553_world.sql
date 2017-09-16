af539fd6ee16fb976b7a63a7e7f90455ac0fcf2b
nbrumbach <nathaniel@websmithgroup.com>
2017-09-16 19:38:50 -0400

    Fix for Otho Mojikos Missing Quests for bug#2588

---
+++ sql/migrations/20170916172553_world.sql
@@ -0,0 +1,9 @@
+UPDATE quest_template
+SET ReqCreatureOrGoCount1 = 8, ReqCreatureOrGoCount2 = 5, ReqCreatureOrGoCount3 = 5, ReqCreatureOrGoCount4 = 2, Type = 0
+WHERE entry = 7841;
+
+UPDATE creature_template
+SET npcflag = 6
+WHERE ENTRY = 14738
+
+
