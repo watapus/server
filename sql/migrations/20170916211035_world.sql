47ecd2940e164880f745fd52b05d597cf2b94bc1
nbrumbach <nathaniel@websmithgroup.com>
2017-09-16 21:46:54 -0400

    Changed all Battle of Hillsbrad Quests to MinLevel 19

--- sql/migrations/20170916211035_world.sql
+++ sql/migrations/20170916211035_world.sql
@@ -1,7 +1,3 @@
 UPDATE quest_template
 SET MinLevel = 19
-WHERE entry = 550;
-
-UPDATE quest_template
-SET MinLevel = 19
-WHERE entry = 567;
+WHERE entry = 550 OR entry = 567 OR entry = 539 OR entry = 532 OR entry = 528 OR entry = 541 OR entry = 529 OR entry = 527;
