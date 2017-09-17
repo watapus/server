35370049fbbc215a3a1da8435c495347a2c090b8
nbrumbach <nathaniel@websmithgroup.com>
2017-09-16 20:53:44 -0400

    Changed Rank of some Molten Core Rare Elites to Elites. Bug#2599

---
+++ sql/migrations/20170916205130_world.sql
@@ -0,0 +1,5 @@
+UPDATE creature_template
+SET rank = 1
+WHERE entry = 11668 OR entry = 11667 OR entry = 11664 OR entry = 11669 OR entry = 12100 OR entry = 12076 OR entry = 11665 OR entry = 12101 OR entry = 11672
+
+
