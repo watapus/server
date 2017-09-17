a2d09a6a748fd84e59a25d2d465d2e956682e908
nbrumbach <nathaniel@websmithgroup.com>
2017-09-16 21:56:20 -0400

    Added INSERT INTO to script

--- sql/migrations/20170916211035_world.sql
+++ sql/migrations/20170916211035_world.sql
@@ -1,3 +1,5 @@
+INSERT INTO `migrations` VALUES ('20170916211035');
+
 UPDATE quest_template
 SET MinLevel = 19
 WHERE entry = 550 OR entry = 567 OR entry = 539 OR entry = 532 OR entry = 528 OR entry = 541 OR entry = 529 OR entry = 527;
